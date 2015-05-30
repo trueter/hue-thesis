113d1ae7fa4a0606b442fb0c5034f96fc0b9ff47
Torsten Rü <torsten@rueter.net>
2015-05-30 20:02:27 +0200

    Initial commit

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name HueThesis.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('HueThesis')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name HueThesis.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('HueThesis')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
